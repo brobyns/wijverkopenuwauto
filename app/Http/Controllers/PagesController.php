ebdf750b6f9f38f1aeb730e9741f38e9e1a838cf
brobyns <bramrobyns@hotmail.com>
2016-11-18 20:05:33 +0100

    npm install

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function earn(){
+        return view('pages.earn');
+    }
+
+    public function faq(){
+        return view('pages.faq');
+    }
+
+    public function tos(){
+        return view('pages.tos');
+    }
+
+    public function privacy(){
+        return view('pages.privacy');
+    }
+
+    public function contact(){
+        return view('pages.contact');
+    }
 }
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bramr
+ * Date: 18/11/2016
+ * Time: 19:36
+ */
